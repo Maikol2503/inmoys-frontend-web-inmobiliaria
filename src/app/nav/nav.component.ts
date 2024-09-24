c30b248f22c05c75f1a2a0b67d4b40736c51744f
maikol <mgarrido2503@gmail.com>
2024-09-24 13:54:31 +0200

    add: section form

---
+++ src/app/nav/nav.component.ts
@@ -0,0 +1,23 @@
+import { Component, HostListener } from '@angular/core';
+
+@Component({
+  selector: 'app-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  scrolled = false;
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+
+    if (scrollPosition > 50) { // Puedes ajustar la cantidad de scroll aquí
+      this.scrolled = true;
+    } else {
+      this.scrolled = false;
+    }
+  }
+}
