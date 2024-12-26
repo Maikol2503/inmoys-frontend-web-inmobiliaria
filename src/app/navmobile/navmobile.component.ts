70bf35958e18ec6b3db5cbd8eae6f9ba324cf143
maikol <mgarrido2503@gmail.com>
2024-12-26 20:16:35 +0100

    add: funtion redirect to navMobile

--- src/app/navmobile/navmobile.component.ts
+++ src/app/navmobile/navmobile.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { ModalMenuMobileServiceService } from '../services/modal-menu-mobile-service.service';
 import { CommonModule } from '@angular/common';
+import {Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navmobile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navmobile.component.html',
   styleUrl: './navmobile.component.css'
 })
@@ -14,7 +15,7 @@ export class NavmobileComponent {
   isVisible = false;
   private baseUrlImagesWeb: string = 'https://inmoys-backend-inmobiliaria-1.onrender.com/images-for-web/';
 
-  constructor(private modalServices:ModalMenuMobileServiceService){
+  constructor(private modalServices:ModalMenuMobileServiceService , private router:Router ){
     // modalServices.modalState$.subscribe((state)=>{
     //   this.isVisible = state
     // })
@@ -31,6 +32,12 @@ export class NavmobileComponent {
 
   closeModal(){
     this.isVisible = false
+  }
+
+
+  redirect(url:string){
+    this.closeModal()
+    this.router.navigate([url])
   }
 
 
